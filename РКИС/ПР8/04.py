92d8fb4e92fe855d87cd05117ecefe532bc329c7
AR867 <120170589+AR867@users.noreply.github.com>
2026-03-31 14:52:09 +0500

    Update 04.py

--- РКИС/ПР8/04.py
+++ РКИС/ПР8/04.py
@@ -1,1 +1,7 @@
+class Printer:
+    def print_message(self, msg):
+        print(f"Message: {msg}")
 
+class UpperCasePrinter(Printer):
+    def print_message(self, msg):
+        print(msg.upper())
