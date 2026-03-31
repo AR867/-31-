5c811ed0da0d2d1e07323777f36e25c1204414e9
AR867 <120170589+AR867@users.noreply.github.com>
2026-03-31 14:47:02 +0500

    Update 03.py

--- РКИС/ПР7/03.py
+++ РКИС/ПР7/03.py
@@ -1,1 +1,9 @@
-
+class UserName:
+    def __init__(self):
+        self.__name = ""
+    
+    def set_name(self, name):
+        self.__name = name
+    
+    def get_name(self):
+        return self.__name.upper()
