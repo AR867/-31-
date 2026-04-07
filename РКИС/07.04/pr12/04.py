60a6aadb7fd3552cf8051c21048f0d33cab8da49
AR867 <120170589+AR867@users.noreply.github.com>
2026-04-07 14:44:57 +0500

    Update 04.py

--- РКИС/07.04/pr12/04.py
+++ РКИС/07.04/pr12/04.py
@@ -1,1 +1,11 @@
+def read_number(s):
+    try:
+        result = int(s)
+        return result
+    except ValueError:
+        return "Error"
+    finally:
+        print("Done")
 
+print(read_number("10"))
+print(read_number("abc"))
