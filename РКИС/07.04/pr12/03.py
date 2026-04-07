de51989b90737d0062ee361d765dfc3a69e31a8d
AR867 <120170589+AR867@users.noreply.github.com>
2026-04-07 14:44:28 +0500

    Update 03.py

--- РКИС/07.04/pr12/03.py
+++ РКИС/07.04/pr12/03.py
@@ -1,1 +1,13 @@
+# ================== Задача 3 — Обработка нескольких исключений ==================
+def safe_operation(a, b):
+    try:
+        return a / b
+    except ZeroDivisionError:
+        return "Division by zero"
+    except TypeError:
+        return "Type error"
 
+# Проверка задачи 3
+print("\n=== Задача 3 ===")
+print(safe_operation(10, 2))
+print
