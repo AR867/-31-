32951e8e5c2b5667f786e89f354e049b93c506af
AR867 <120170589+AR867@users.noreply.github.com>
2026-04-07 14:43:39 +0500

    Update 01.py

--- РКИС/07.04/pr12/01.py
+++ РКИС/07.04/pr12/01.py
@@ -1,1 +1,11 @@
+# ================== Задача 1 — Перехват ошибки деления ==================
+def safe_divide(a, b):
+    try:
+        return a / b
+    except ZeroDivisionError:
+        return "Division by zero"
 
+# Проверка задачи 1
+print("=== Задача 1 ===")
+print(safe_divide(10, 2))
+print(safe_divide(5, 0))
