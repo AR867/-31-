ebcf6c3a4360ca6dc767a5649e627a079a9867ff
AR867 <120170589+AR867@users.noreply.github.com>
2026-04-07 14:44:01 +0500

    Update 02.py

--- РКИС/07.04/pr12/02.py
+++ РКИС/07.04/pr12/02.py
@@ -1,1 +1,11 @@
+# ================== Задача 2 — Перехват ошибки преобразования ==================
+def to_int(s):
+    try:
+        return int(s)
+    except ValueError:
+        return "Invalid input"
 
+# Проверка задачи 2
+print("\n=== Задача 2 ===")
+print(to_int("123"))
+print(to_int("abc"))
